0c42ff7a7e055f99c424ec6a3bf96d29aa3bceda
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-07-25 17:37:25 -0600

    Add user saving.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,13 @@ const userSignIn = async (req, res) => {
 	res.json({ message: 'user sign in' })
 }
 const userSignUp = async (req, res) => {
-	res.json({ message: 'user sign up' })
+	const { email, password } = req.body
+	try {
+		const user = await User.signup(email, password)
+		res.status(200).json({ email, user })
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
 }
 
 module.exports = {
